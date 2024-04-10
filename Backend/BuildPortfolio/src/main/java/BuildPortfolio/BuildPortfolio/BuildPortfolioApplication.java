f8665c2fd23ace868410751517555f0080c1303c
Saad Ali <mdsaadali01298@gmail.com>
2024-04-10 13:16:16 +0530

    Basic Working Code
    - draw.io file added
    
    - Angular Basic Code (using Node: 20.11.1, npm: 9.5.0, Angular: 14.2.13, and rxjs: 7.5.7)
    
    - Spring Boot Basic Code (using Java: 17, Spring Boot: 3.2.0)

---
+++ Backend/BuildPortfolio/src/main/java/BuildPortfolio/BuildPortfolio/BuildPortfolioApplication.java
@@ -0,0 +1,13 @@
+package BuildPortfolio.BuildPortfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildPortfolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuildPortfolioApplication.class, args);
+	}
+
+}
